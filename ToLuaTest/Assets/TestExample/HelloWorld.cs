4b39dcda14252daf88ce70a8c5932d0f6f238b2f
Carol <475880692@qq.com>
2018-05-18 14:49:54 +0800

    eg.: HelloWorld.cs

--- ToLuaTest/Assets/TestExample/HelloWorld.cs
+++ ToLuaTest/Assets/TestExample/HelloWorld.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using LuaInterface;
+public class HelloWorld : MonoBehaviour
+{
 
-public class HelloWorld : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    /*
+	1.使用Tolua的相关类及方法都需要调用命名空间LuaInterface
+	2.调用lua脚本必须要先创建一个lua的虚拟机，而创建的步骤就是LuaState lua = new LuaState();，
+	创建一个LuaState型的对象，这个就是lua的虚拟机，后面的lua的与C#的交互全部依仗这个东西。
+    3.lua中直接运行一段lua脚本最简单的方法就是 lua.DoString
+	4.使用完lua虚拟机之后记得要销毁，具体操作如下：先进行lua虚拟机栈的判空，具体对应的就是lua.CheckTop ，
+	  然后就是析构掉lua虚拟机，具体方法为lua.Dispose
+	*/ 
+    private void Awake()
+    {
+	   LuaState  lua = new LuaState();
+	   lua.Start();
+	   string hello = @"print('hello tolua#')";
+	   string hello1 = "print('what')";
+	   lua.DoString(hello1,"HelloWorld.cs");
+	   lua.CheckTop();
+	   lua.Dispose();
+	   lua = null;
+    }
 }
